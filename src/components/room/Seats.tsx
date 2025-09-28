18f7ca0912b31752e2f4f8f5b7dd45dc46619f59
Aseel Alwjeeh <aseelalwjeeh6@gmail.com>
2025-09-28 15:07:13 +0000

    المقاعد 4

--- src/components/room/Seats.tsx
+++ src/components/room/Seats.tsx
@@ -69,7 +69,7 @@ const Seats = ({ members }: { members: Member[] }) => {
       </CardHeader>
       <CardContent className="p-4">
         {members.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+          <div className="grid grid-cols-4 gap-8">
             {members.map((member) => (
               <Seat key={member.name} member={member} participant={getParticipant(member.name)} />
             ))}
