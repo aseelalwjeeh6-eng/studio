2a8c3a732a16b10c667133eedac235ae04920637
Aseel Alwjeeh <aseelalwjeeh6@gmail.com>
2025-10-02 15:44:21 +0000

    هذه ايضا اعدادات firbase. Firebase app Web App App nickname Firebase ap

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,15 @@ import { getDatabase, type Database } from "firebase/database";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  "projectId": "studio-2181157065-1eeef",
-  "appId": "1:441437458465:web:e567aaf859099360",
   "apiKey": "AIzaSyD9NXHZIQgnKNwCBsPTB3dbnBbx6MMFeuc",
   "authDomain": "studio-2181157065-1eeef.firebaseapp.com",
-  "databaseURL": "https://studio-2181157065-1eeef-default-rtdb.firebaseio.com",
-  "storageBucket": "studio-2181157065-1eeef.appspot.com",
+  "databaseURL": "https://studio-2181157065-1eeef-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "studio-2181157065-1eeef",
+  "storageBucket": "studio-2181157065-1eeef.firebasestorage.app",
   "messagingSenderId": "441437458465",
-  "measurementId": "G-5G3W9K5153"
+  "appId": "1:441437458465:web:e567aaf8547cf859099360"
 };
+
 
 // Initialize Firebase
 const app: FirebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
